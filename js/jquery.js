2006a7fd3920f3f59df0d92289fedff8ce5d6149
diarmuid.nash <diarmuidn7@gmail.com>
2016-06-10 14:33:44 +0100

    initial commit

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+#carman.jpg{
+    width:340px;
+}
+
+<select name="carman.jpg" id="carman.jpg">
+    <option value="carman.jpg" title="Checkout our forum"></option>
+    <option value="carman.jpg" title="In the Media"></option>
+    <option value="carman.jpg" title="Green Policy"></option>
+</select>
+
+$("body select").msDropDown();
